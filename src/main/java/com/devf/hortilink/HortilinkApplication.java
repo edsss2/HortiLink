6c9428a6ca806bedaeb99ea57dbb909e09bad2be
edsss2 <edsonsanchezveiga@gmail.com>
2025-10-11 18:04:13 -0300

    Inicializa back-end Java

---
+++ src/main/java/com/devf/hortilink/HortilinkApplication.java
@@ -0,0 +1,13 @@
+package com.devf.hortilink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortilinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortilinkApplication.class, args);
+	}
+
+}
