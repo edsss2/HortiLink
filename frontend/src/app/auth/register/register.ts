d6f64488f8a8b747af297eb45b994338be5b668f
Edson Veiga <edsonsanchezveiga@gmail.com>
2025-10-28 16:14:00 -0300

    Começa a estilizar a tela de register

--- frontend/src/app/auth/register/register.ts
+++ frontend/src/app/auth/register/register.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
+  onSubmit() {
+    
+  }
 }
