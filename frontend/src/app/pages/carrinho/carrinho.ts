e8a200eb404eaf1ba9bfd5f6a7431227b2a1a2c7
Edson Veiga <edsonsanchezveiga@gmail.com>
2025-11-12 16:24:11 -0300

    Começa a estruturação da página carrinho

--- frontend/src/app/pages/carrinho/carrinho.ts
+++ frontend/src/app/pages/carrinho/carrinho.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../services/usuario-service';
 import { Navbar } from '../../components/navbar/navbar.component';
 import { Footer } from "../../components/footer/footer.component";
+import { Produto } from '../../models/produto.model';
+import { ProdutoService } from '../../services/produto-service';
 
 @Component({
   selector: 'app-carrinho',
@@ -9,15 +11,30 @@ import { Footer } from "../../components/footer/footer.component";
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
-export class Carrinho {
+export class Carrinho implements OnInit {
 
   carrinho: number[] = [];
   carrinhoCount: number = 0;
+  produtos: Produto[] = [];
 
-  constructor(private usuarioService : UsuarioService){}
+  constructor(private usuarioService : UsuarioService, private produtoService : ProdutoService){}
 
+  ngOnInit(): void {
+      this.buscarCarrinho();
+  }
   get usuarioLogado() {
     return this.usuarioService.getCurrentUser();
+  }
+
+  private buscarCarrinho() {
+    return this.produtoService.getCarrinho(this.carrinho).subscribe(
+      (resposta) => {
+        this.produtos = resposta;
+      },
+      (erro) => {
+        console.log("Ops, algo deu errado:", erro);
+      }
+    );
   }
 
 }
