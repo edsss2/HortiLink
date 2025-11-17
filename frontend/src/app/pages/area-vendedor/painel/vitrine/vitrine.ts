095e1faa3604769c85a6e7949a3dae4b44f20e4b
Edson Veiga <edsonsanchezveiga@gmail.com>
2025-11-17 19:48:12 -0300

    Corrije areas que ainda estão em desenvolvimento

--- frontend/src/app/pages/area-vendedor/painel/vitrine/vitrine.ts
+++ frontend/src/app/pages/area-vendedor/painel/vitrine/vitrine.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-vitrine',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './vitrine.html',
   styleUrl: './vitrine.css',
 })
